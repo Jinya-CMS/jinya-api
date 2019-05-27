63cf36a4f32e81fd08fc8ab4d8ca119f951bd50f
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2019-05-27 04:48:02 +0200

    Added Dockerfile

--- app.go
+++ app.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"net/http"
-	"net/http/cgi"
-	"os"
-
 	"./bug"
 	"./feature"
 	"./like"
+	"net/http"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/julienschmidt/httprouter"
@@ -19,17 +16,8 @@ func main() {
 	router.POST("/tracker/feature", feature.Route)
 	router.POST("/tracker/like", like.Route)
 
-	if len(os.Args) > 1 && os.Args[1] == "self-hosted" {
-		err := http.ListenAndServe(":8090", router)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		os.Setenv("REQUEST_URI", os.Getenv("REDIRECT_URL"))
-
-		err := cgi.Serve(router)
-		if err != nil {
-			panic(err)
-		}
+	err := http.ListenAndServe(":8090", router)
+	if err != nil {
+		panic(err)
 	}
 }
